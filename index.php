097f299ca49f8fbbafa5c2ab9dca60399838b86f
Muhammad Sibtain <mohsibtain@gmail.com>
2024-08-15 18:13:03 +0500

    profile create, attach profile to list

--- index.php
+++ index.php
@@ -19,8 +19,12 @@ class GravityFormsToKlaviyo
     function __construct() 
     {
         $this->klaviyo_api_key = get_option("klaviyo_api_key");
-
         add_action( 'gform_entry_post_save', [$this, 'ilab_entry_post_save_handler'], 10, 2 );   
+    }
+
+    function keepOnlyNumbers($input) 
+    {
+        return preg_replace('/\D/', '', $input);
     }
 
     function ilab_entry_post_save_handler($entry, $form)
@@ -35,21 +39,17 @@ class GravityFormsToKlaviyo
             $phone              = $entry['7'];
             $mailing_address    = $entry['8'];
 
-            //   '{"data":{"type":"profile","attributes":{"properties":{"newKey":"New Value"}},"meta":{"patch_properties":{"append":{"newKey":"New Value"},"unappend":{"newKey":"New Value"}}}}}',
             $body = [
                 'data' => [
                     'type' => 'profile',
                         'attributes' => [
-                            "title" => "Regional Manager",
                             "first_name" => sanitize_text_field($first_name),
                             "last_name" => sanitize_text_field($last_name),
                             "email" => sanitize_text_field($email),
-                            "phone_number" => "+1234567890"
+                            "phone_number" => "+1" . $this->keepOnlyNumbers( $phone )
                     ]
                 ]
-            ];            
-
-            p_r($body);
+            ];
 
             $klaviyo = new KlaviyoAPI(
                 $this->klaviyo_api_key, 
@@ -66,39 +66,32 @@ class GravityFormsToKlaviyo
             } 
             catch (Exception $exception) 
             {
-                echo $exception;
+                //die( $exception );
             }
             
+            # Add Profile to the List;
+            if (isset($response['data']['id']))
+            {
+                $body = [
+                    'data' => [
+                        [
+                            'type' => 'profile',
+                            'id' => $response['data']['id']
+                        ]
+                    ]
+                ];
 
-            //$response = $klaviyo->Lists->createListRelationships($this->list_id, $data);
-        
-            /*
-            //$data = json_encode($data);
-        
-            $curl = curl_init();
-        
-            curl_setopt_array($curl, [
-              CURLOPT_URL => "https://a.klaviyo.com/api/v2/list/" . $this->list_id . "/members?api_key=" . $this->klaviyo_api_key,
-              CURLOPT_RETURNTRANSFER => true,
-              CURLOPT_ENCODING => "",
-              CURLOPT_MAXREDIRS => 10,
-              CURLOPT_TIMEOUT => 30,
-              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-              CURLOPT_CUSTOMREQUEST => "POST",
-              CURLOPT_POSTFIELDS => $data,
-              CURLOPT_HTTPHEADER => [
-                "Accept: application/json",
-                "Content-Type: application/json"
-              ],
-            ]);
-        
-            $response = curl_exec($curl);
-            $err = curl_error($curl);
-            */
-
-            p_r($response);
-
-            exit;
+                
+                try 
+                {
+                    $response = $klaviyo->Lists->createListRelationships($this->list_id, $body);
+                } 
+                catch (Exception $exception) 
+                {
+                    //die( $exception );
+                }
+                
+            }
         }
     }
 }
